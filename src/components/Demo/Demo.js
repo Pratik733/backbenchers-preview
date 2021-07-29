5795b323f8e24734d224a7073a3d2cae0e1bc624
Ashutosh <vu4f1920027@pvppcoe.ac.in>
2021-07-29 12:05:08 +0530

    added scroll to top

--- src/components/Demo/Demo.js
+++ src/components/Demo/Demo.js
@@ -7,18 +7,18 @@ const Demo = () => {
             <div className="demo ">
                 <div class="demo-container mx-auto flex px-5 py-10 md:flex-row flex-col items-center">
                     <div class="demo-contents lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center">
-                        <h1 class="title-font sm:text-4xl text-3xl  font-medium text-gray-400">Beta Testing Phase 2
+                        <h1 class="title-font sm:text-4xl text-3xl  font-medium text-gray-100">Beta Testing Phase 2
                         </h1>
-                        <p class=" leading-relaxed text-gray-200 w-4/5">We are back with our exclusive beta testing 2 along with  some exciting offers
+                        <p class=" leading-relaxed text-gray-300 w-4/5 pt-2">We are back with our exclusive beta testing 2 along with  some exciting offers
                             Moreover, we have added one more option with : </p>
                         <div className="">
-                            <ul className="list-disc text-white ml-5">
-                                <li>CODING</li>
+                            <ul className="list-disc text-gray-300 ml-5">
+                                <li >CODING</li>
                                 <li>MATHEMATICS TIPS & TRICKS</li>
                             </ul>
                             <div className="py-5">
                                 <h1 className="text-2xl font-bold text-yellow-500">Course Contents</h1>
-                                <ul className="list-disc px-10 text-indigo-100">
+                                <ul className="list-disc px-10 text-gray-300">
                                     <li>1:1 Live classes</li>
                                     <li>Recorded Sessions</li>
                                     <li>Conceptual Test</li>
@@ -31,13 +31,13 @@ const Demo = () => {
                                     <li>Alumni Network</li>
                                     <li>India's First Gamified Virtual Campus</li>
                                     <li>Seminars/webinars/Fests/Events</li>
-                                    <li>& many more......</li>
+                                    <li>& many more...</li>
                                 </ul>
                             </div>
                             <h1 className="text-2xl font-bold text-yellow-500">Subscription Fees</h1>
-                            <div className="py-5">
-                                <ul className="price-list ">
-                                    <li className="text-white">For CODING<span className="subs-price text-red-400"> ₹99/Student</span></li>
+                            <div className="py-2">
+                                <ul className="price-list text-gray-300">
+                                    <li>For CODING<span className="subs-price text-red-400"> ₹99/Student</span></li>
                                     <li>For MATHEMATICS TIPS & TRICKS<span className="subs-price text-red-400">₹99/Student</span></li>
                                     <li>For COMBO COURSE REGISTRATION<br/>[ CODING + MATHEMATICS TIPS & TRICKS ] <span className="subs-price text-red-400">₹149/Student</span></li>
                                     <h1 className="py-5 pt-16 font-medium text-lg text-white">Special Discount For Group Registration[Only valid for Combo Course Registration]</h1>
@@ -45,9 +45,7 @@ const Demo = () => {
                                     <li>Registration of 4 Students <span className="subs-price text-red-400">₹99/student</span></li>
                                     <li>Registration of 5+ students<span className="subs-price text-red-400">₹79/student</span></li>
                                 </ul>
-
                             </div>
-
                         </div>
                         <div class="flex justify-center w-4/5">
                             <a target='_blank' href="https://backbenchers-edtech.typeform.com/single-course">
@@ -57,10 +55,9 @@ const Demo = () => {
                                 <button class="ml-4 inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-400 rounded text-lg">Combo Courses</button>
                             </a>
                         </div>
-                        <div className="w-3/4 py-5">
+                        <div className="w-3/4 py-5 pt-10">
                             <h1 className="text-2xl text-white">What’s more?</h1>
                             <p className="text-gray-400">All the proceedings[Subscription Fees] will be donated for a social cause which will either be for flood relief  or Orphanage or both</p>
-
                         </div>
                     </div>
                 </div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Logo from '../assets/images/Logo.png'
 import './Footer.css'
 import { Link } from 'react-router-dom';
+import ScrollToTop from "react-scroll-to-top";
+import { FaChevronUp } from "react-icons/fa";
 
 const Footer = () => {
     return (
@@ -15,41 +17,44 @@ const Footer = () => {
                     </div>
                     <div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
                         <div class="lg:w-1/3 md:w-1/2 w-full px-5">
-                            <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">COMPANY</h2>
+                            <h2 class="title-font font-medium text-white tracking-widest text-md mb-3">Company</h2>
                             <nav class="list-none mb-10">
                                 <li>
-                                    <a class="text-gray-400 hover:text-white" href='#'>Discord Server</a>
+                                    <a class="text-gray-400 hover:text-white" href='https://discord.gg/uubrUH8eRW'>Discord Server</a>
                                 </li>
                                 <li>
-                                    <a class="text-gray-400 hover:text-white" href='#'>Whatsapp</a>
+                                    <a class="text-gray-400 hover:text-white" href='https://chat.whatsapp.com/Cf7QS5vT6lpB4eEaCzjLwF'>Whatsapp</a>
                                 </li>
                                 <li>
-                                    <a class="text-gray-400 hover:text-white" href='#'>Euphoria</a>
+                                    <a class="text-gray-400 hover:text-white" href='#'>Euphoria (Coming Soon) </a>
                                 </li>
                                 <li>
-                                    <a class="text-gray-400 hover:text-white" href='#'>Work with us</a>
+                                    <a class="text-gray-400 hover:text-white" href='https://backbenchers-edtech.typeform.com/work-with-us'>Work with us</a>
                                 </li>
                                 <li>
-                                    <a class="text-gray-400 hover:text-white" href='#'>Gamified Learning</a>
+                                    <a class="text-gray-400 hover:text-white" href='https://www.instagram.com/backbenchersed.tech/'>Gamified Learning</a>
                                 </li>
                                 <li>
-                                    <a class="text-gray-400 hover:text-white" href='#'>Apply for Students Developer Program</a>
+                                    <a class="text-gray-400 hover:text-white" href='https://backbenchers-edtech.typeform.com/student-dvlp-pg'>Apply for Students Developer Program</a>
                                 </li>
                             </nav>
                         </div>
                         <div class="lg:w-1/3 md:w-1/2 w-full px-5">
-                            <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">Products</h2>
+                            <h2 class="title-font font-medium text-white tracking-widest text-md mb-3">Products</h2>
                             <nav class="list-none mb-10">
                                 <li>
-                                    <a class="text-gray-400 hover:text-white" href='#'>Meme Cheatsheets</a>
+                                    <a class="text-gray-400 hover:text-white" href='https://drive.google.com/drive/folders/1eu7RkTDVTfNsWNs0ak3IDRP9-wnfewgf?usp=sharing'>Meme Cheatsheets</a>
                                 </li>
                                 <li>
-                                    <a class="text-gray-400 hover:text-white" href='#'>Interactive Classes</a>
+                                    <a class="text-gray-400 hover:text-white" href='https://drive.google.com/drive/folders/1NK86QICo0qYRvJ1TQe05p5r_8ESaWZHT?usp=sharing'>Interactive Classes</a>
+                                </li>
+                                <li>
+                                    <a class="text-gray-400 hover:text-white" href='#'>Book A Walk Through</a>
                                 </li>
                             </nav>
                         </div>
                         <div class="lg:w-1/3 md:w-1/2 w-full px-5">
-                            <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">About</h2>
+                            <h2 class="title-font font-medium text-white tracking-widest text-md mb-3">About</h2>
                             <nav class="list-none mb-10">
                                 <li>
                                     <Link
@@ -103,24 +108,25 @@ const Footer = () => {
                 </div>
                 <div class="bg-gray-800 bg-opacity-75">
                     <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-                        <p class="text-gray-400 text-sm text-center sm:text-left">© Copyrights 2021 —
-                            <a href="#" rel="noopener noreferrer" class="text-gray-500 ml-1" target="_blank">@BackBenchers Edtech</a>
+                        <p class="text-gray-400 text-md text-center sm:text-left">© Copyrights 2021 —
+                            <a href="https://www.instagram.com/backbenchersed.tech/" rel="noopener noreferrer" class="text-gray-500 ml-1" target="_blank">@BackBenchers Edtech</a>
                         </p>
                         <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-                            <a class="text-gray-400 hover:text-white" href="#">
+                            <a class="text-gray-400 hover:text-white" href="https://www.facebook.com/BackBenchers-Edtech-105416761838435/?view_public_for=105416761838435">
                                 <i class="fab fa-facebook-f"></i>
                             </a>
-                            <a class="ml-3 text-gray-400 hover:text-white" href="#">
+                            <a class="ml-3 text-gray-400 hover:text-white" href="https://twitter.com/BackBenchersEd">
                                 <i class="fab fa-twitter"></i>
                             </a>
-                            <a class="ml-3 text-gray-400 hover:text-white" href="#">
+                            <a class="ml-3 text-gray-400 hover:text-white" href="https://www.instagram.com/backbenchersed.tech/">
                                 <i class="fab fa-instagram"></i>
                             </a>
-                            <a class="ml-3 text-gray-400 hover:text-white" href="#">
+                            <a class="ml-3 text-gray-400 hover:text-white" href="https://www.linkedin.com/company/backbenchers-edtech">
                                 <i class="fab fa-linkedin-in"></i>
                             </a>
                         </span>
                     </div>
+                    <ScrollToTop smooth component={<FaChevronUp/>} style={{color:'#db9501', background:'#1e434c', padding: '12px', boxShadow: '0px 0px 5px rgba(0,0,0,.5)'}} />
                 </div>
             </footer>
         </div>
