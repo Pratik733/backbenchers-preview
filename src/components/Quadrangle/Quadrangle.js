3da22cc5e3e36a1a79d145f7e5a25f80df603241
Ashutosh <vu4f1920027@pvppcoe.ac.in>
2021-08-17 17:08:45 +0530

    updated bugs

--- src/components/Quadrangle/Quadrangle.js
+++ src/components/Quadrangle/Quadrangle.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import "./Quadrangle.css";
-import quad from  '../assets/images/quad.mp4'
 
 const Quadrangle = () => {
   return (
     <div>
       <div className="quad">
-      <video src={quad} autoplay="true" />
       </div>
     </div>
   );
